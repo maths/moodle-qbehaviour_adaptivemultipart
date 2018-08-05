3b2a800988831dbe89c5d0d0ce517c4490b53c43
Hung Tran <Hung.TranMinh1@Nashtechglobal.com>
2018-08-05 15:12:34 +0700

    GDPR Implementation null_provider

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -13,14 +13,29 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
- * Strings for component 'qbehaviour_adaptive', language 'en'.
+ * Privacy Subsystem implementation for qbehaviour_adaptivemultipart.
  *
  * @package    qbehaviour_adaptivemultipart
- * @copyright  2012 The Open University
+ * @copyright  2018 The Open University 
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-$string['pluginname'] = 'Adaptive mode (multi-part questions)';
-$string['privacy:metadata'] = 'The Adaptive mode (multi-part questions) plugin does not store any personal data.';
+namespace qbehaviour_adaptivemultipart\privacy;
+defined('MOODLE_INTERNAL') || die();
+/**
+ * Privacy Subsystem for qbehaviour_adaptivemultipart implementing null_provider.
+ *
+ * @copyright  2018 The Open University 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata';
+    }
+}
